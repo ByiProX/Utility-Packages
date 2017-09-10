033619a545b1769721dff8a51c32fcd7df6af8cd
Quentin Wang <wangkx0105@outlook.com>
2017-09-10 23:12:20 +0800

    Update img2txt.py

--- img2txt.py
+++ img2txt.py
@@ -3,15 +3,15 @@
 from PIL import Image
 import argparse
 
-#命令行输入参数处理
+# 命令行输入参数处理
 parser = argparse.ArgumentParser()
 
-parser.add_argument('file')     #输入文件
-parser.add_argument('-o', '--output')   #输出文件
-parser.add_argument('--width', type = int, default = 80) #输出字符画宽
-parser.add_argument('--height', type = int, default = 80) #输出字符画高
+parser.add_argument('file')     # 输入文件
+parser.add_argument('-o', '--output')   # 输出文件
+parser.add_argument('--width', type = int, default = 80) # 输出字符画宽
+parser.add_argument('--height', type = int, default = 80) # 输出字符画高
 
-#获取参数
+# 获取参数
 args = parser.parse_args()
 
 IMG = args.file
